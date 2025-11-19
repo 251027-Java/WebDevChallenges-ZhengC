062f4b11b85996983fa5809968d893b65b6fd07b
zhchgatech <zhchgatech@gmail.com>
2025-11-19 15:18:45 -0600

    task11

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-function greetUser() {
-  let name = document.getElementById("nameInput").value;
-  
-  let color = document.getElementById("colorInput").value;
-  if (name.trim() === "") {
-    alert("Please enter your name first!");
-  } else {
-    alert("Hello, " + name +" like color "+ color+"!");
-  }
+function addNumbers() {
+  let n1 = Number(document.getElementById("num1").value);
+  let n2 = Number(document.getElementById("num2").value);
+  let sum = n1 + n2;
+
+  document.getElementById("result").textContent = "Result: " + sum;
+}
+function subNumbers() {
+  let n1 = Number(document.getElementById("num1").value);
+  let n2 = Number(document.getElementById("num2").value);
+  let sum = n1 - n2;
+
+  document.getElementById("result").textContent = "Result: " + sum;
+}
+function mulNumbers() {
+  let n1 = Number(document.getElementById("num1").value);
+  let n2 = Number(document.getElementById("num2").value);
+  let sum = n1 * n2;
+
+  document.getElementById("result").textContent = "Result: " + sum;
+}
+function divNumbers() {
+  let n1 = Number(document.getElementById("num1").value);
+  let n2 = Number(document.getElementById("num2").value);
+  let sum = n1 / n2;
+
+  document.getElementById("result").textContent = "Result: " + sum;
 }
 
-document.getElementById("greetBtn").addEventListener("click", greetUser);
+document.getElementById("addBtn").addEventListener("click", addNumbers);
+document.getElementById("addBtns").addEventListener("click", subNumbers);
+document.getElementById("addBtnm").addEventListener("click", mulNumbers);
+document.getElementById("addBtnd").addEventListener("click", divNumbers);
+
